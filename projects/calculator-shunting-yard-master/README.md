0991256c9b0b087781daf75475b53a1dfd1092f9
Damian Rieumont <damianaramis00@gmail.com>
2023-10-10 15:22:56 -0400

    Adding calculator project

---
+++ projects/calculator-shunting-yard-master/README.md
@@ -0,0 +1,2 @@
+# calculator-shunting-yard
+Calculator web app with shunting yard algorithm solution and a very powerful responsive design
---
+++ projects/calculator-shunting-yard-master/modules/data-struct/stack.js
@@ -0,0 +1,41 @@
+export class Stack {
+    constructor() {
+        this._items = [];
+    }
+
+    push(item) {
+        //push item to the stack
+        this._items.push(item);
+        return this._items;
+
+    }
+
+    pop() {
+        //pull out the topmost item (last item) from stack
+        return this._items.pop()
+    }
+
+    peek() {
+        // see what's the last item in stack
+        return this._items[this._items.length - 1]
+    }
+
+    size() {
+        //no. of items in stack
+        return this._items.length
+    }
+
+    isEmpty() {
+        // return whether the stack is empty or not
+        return this._items.length == 0
+    }
+
+    print() {
+        console.log(this._items);
+    }
+
+    toArray() {
+        return this._items;
+    }
+
+}
